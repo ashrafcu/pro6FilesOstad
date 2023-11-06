7350e87ae1702ee6fb3b6bd7213134f7a6d6f586
Ashraf Ahmed <ahmashraf@gmail.com>
2023-11-06 23:55:40 +0600

    Updated Query

--- index.php
+++ index.php
@@ -1,16 +1,42 @@
-<?php include "header.php";?>
+<?php include "header.php";
+$hostname = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'appm6pfiles';
+$mysqli = new mysqli($hostname, $username, $password, $database);
+?>
 
-  <!-- HEADER -->
+<!-- HEADER -->
 
-  <!-- MAIN -->
-  <main role="main">
-    <!-- TOP SECTION -->
+<!-- MAIN -->
+<main role="main">
+  <!-- TOP SECTION -->
+  <?php
+  $sql = "SELECT * FROM orders";
+  $myOrders = $mysqli->query($sql);
+  ?>
+<h2>Total Number of Orders by Each Customer</h2>
+  <table>
+    <tr>
+      <th>Order ID</th>
+      <th>Order Date</th>
+    </tr>
+    <?php
+    while ($row = $myOrders->fetch_assoc()) {
+      ?>
+      <tr>
+        <td>
+          <?php echo $row['order_id']; ?>
+        </td>
+        <td>
+          <?php echo $row['order_date']; ?>
+        </td>
+      </tr>
+    <?php } ?>
+  </table>
 
-    <!-- SECTION 1 -->
-    <section class="grid margin text-center">
-    
-    </section>
-  </main>
-<?php include "footer.php";?>
-
-  
+  <!-- SECTION 1 -->
+  <section class="grid margin text-center">
+  </section>
+</main>
+<?php include "footer.php"; ?>
